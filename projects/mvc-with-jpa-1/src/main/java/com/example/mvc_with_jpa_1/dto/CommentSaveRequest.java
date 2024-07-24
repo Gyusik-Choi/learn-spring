dd7f772580edc64ede03113debb321cb6705314e
최 규식 <kuidoli@hanmail.net>
2024-07-24 12:18:32 +0900

    Feat: add NoArgsConstructor annotation for ObjectMapper

--- projects/mvc-with-jpa-1/src/main/java/com/example/mvc_with_jpa_1/dto/CommentSaveRequest.java
+++ projects/mvc-with-jpa-1/src/main/java/com/example/mvc_with_jpa_1/dto/CommentSaveRequest.java
@@ -2,16 +2,20 @@ package com.example.mvc_with_jpa_1.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentSaveRequest {
 
 //    https://www.inflearn.com/questions/16953/11-16-%EC%A7%88%EB%AC%B8%EC%9E%85%EB%8B%88%EB%8B%A4
 //    @NotEmpty
     @NotNull
-    private Long postId;
+    public Long postId;
 
     @NotEmpty
-    private String content;
+    public String content;
 }
