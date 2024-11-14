194add76605f8fde6f40c0a6d7f0ca551d1064ac
최 규식 <kuidoli@hanmail.net>
2024-11-14 15:45:35 +0900

    Chore: add explanation

--- coupon-1/src/main/java/com/example/coupon_1/controller/HelloController.java
+++ coupon-1/src/main/java/com/example/coupon_1/controller/HelloController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        // 초당 2건을 처리 * 200 (서버에서 동시에 처리할 수 있는 수 -> 톰캣 스레드 풀의 최대 스레드 갯수 200개) = 400
+        // locust 로 성능 테스트를 하면 RPS 가 대략 400 이 나옴
+        Thread.sleep(500);
         return "hello";
     }
 }
