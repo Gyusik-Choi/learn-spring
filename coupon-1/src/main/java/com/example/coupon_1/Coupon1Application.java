9f025c6a914c8a53834d742f83acc7ab297a6a11
최 규식 <kuidoli@hanmail.net>
2024-11-20 12:10:34 +0900

    Feat: add @EnableJpaAuditing annotation to use BaseTimeEntity

--- coupon-1/src/main/java/com/example/coupon_1/Coupon1Application.java
+++ coupon-1/src/main/java/com/example/coupon_1/Coupon1Application.java
@@ -2,8 +2,10 @@ package com.example.coupon_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Coupon1Application {
 
 	public static void main(String[] args) {
