67a7db4f9ccdaf26dbf6cada34f2c4464a62a5df
최 규식 <kuidoli@hanmail.net>
2024-11-25 12:05:42 +0900

    Feat: add ActiveProfiles annotation for test

--- coupon-1/src/test/java/com/example/coupon_1/Coupon1ApplicationTests.java
+++ coupon-1/src/test/java/com/example/coupon_1/Coupon1ApplicationTests.java
@@ -2,7 +2,10 @@ package com.example.coupon_1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Coupon1ApplicationTests {
 
