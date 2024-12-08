e20dffd592c6a00a6805866fea38dfbe289ea8ff
최 규식 <kuidoli@hanmail.net>
2024-12-09 08:35:07 +0900

    Docs: non atomic logic inside redis distributed lock

--- coupon-1/src/main/java/com/example/coupon_1/service/AsyncCouponIssueService.java
+++ coupon-1/src/main/java/com/example/coupon_1/service/AsyncCouponIssueService.java
@@ -43,7 +43,6 @@ public class AsyncCouponIssueService {
         coupon.checkIssuableCoupon();
 
         asyncCouponValidator.checkAvailableCouponIssue(coupon, userId);
-
         saveCouponIssue(couponId, userId);
     }
 
@@ -69,6 +68,26 @@ public class AsyncCouponIssueService {
 
         coupon.checkIssuableCoupon();
 
+        /*
+          distributeLockExecutor 분산락 안에서 진행되는 로직
+          1. totalQuantity > redisRepository.sCard(key); // 쿠폰 발급 수량 제어
+          2. !redisRepository.sIsMember(key, String.valueOf(userId)); // 중복 발급 요청 제어
+          3. redisRepository.sAdd // 쿠폰 발급 요청 저장
+          4. redisRepository.rPush // 쿠폰 발급 큐 적재
+
+          https://tjdrnr05571.tistory.com/18
+          (distributeLockExecutor) 레디스 분산락을 사용하지 않으면 동시성 이슈가 일어날 수 있디
+          레디스가 싱글 스레드로 동작하는데 동시성 이슈가 왜 발생할 수 있는지 궁금했는데 위 글이 이를 이해하는데 도움이 됐다
+          단일 연산이라면 분산락 없이 동시성 이슈가 발생하지 않았겠지만 여러 연산이 수행되기 때문에
+          싱글 스레드라고 하더라도 동시성 이슈가 발생할 수 있다
+          위의 1, 2 연산에서 정합성을 검증하고 3, 4 연산에서 쿠폰 발급을 처리한다
+          특정 스레드의 1, 2 연산과 3, 4 연산 사이에 다른 여러 스레드들이 1, 2 연산을 수행하게 되면
+          1, 2 연산은 모두 통과하게 돼서 3, 4 연산도 문제없이 발생하게 된다
+          그래서 동시성 이슈가 생길 수 있다
+
+          -> 성능 개선 및 동시성 이슈 개선을 위해 distributeLockExecutor 대신
+          redis script 를 통해 4개 작업을 원자성을 가진 1개 작업으로 전환
+         */
         distributeLockExecutor.execute("lock %s".formatted(couponId), 3000, 3000, () -> {
             asyncCouponValidator.checkAvailableCouponIssue(coupon, userId);
             saveCouponIssue(couponId, userId);
